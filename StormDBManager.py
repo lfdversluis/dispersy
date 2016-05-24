2ef17f3676b7b2891df8520ff5e3f676765065b1
lfdversluis <laurens-versluis@hotmail.com>
2016-05-24 13:08:18 +0200

    removed the decorators and call run directly now. Added a wrapper for the execute to ensure no database cursor will be returned by the run method. Changed doc accordingly

--- StormDBManager.py
+++ StormDBManager.py
@@ -1,5 +1,5 @@
 from storm.locals import *
-from storm.twisted.transact import transact, Transactor
+from storm.twisted.transact import Transactor
 from twisted.internet import reactor
 
 
@@ -22,7 +22,6 @@ class StormDBManager:
         # The transactor is required when you have methods decorated with the @transact decorator
         self.transactor = Transactor(reactor.getThreadPool())
 
-    @transact
     def execute_query(self, query, arguments = None):
         """
         Executes a query on the twisted thread-pool using the storm framework.
@@ -31,15 +30,22 @@ class StormDBManager:
         :return: None as this is function is executed on the thread-pool, database objects
         such as cursors cannot be returned.
         """
-        self.database._cursor.execute(query, arguments)
 
-    @transact
+        def _wrap(sql_query, query_arguments):
+            """
+            This function wraps the execute function to ensure None is returned as the
+            self.transactor.run method returns the result of the execution.
+            """
+            self.transactor.run(self.database._cursor.execute(sql_query, query_arguments))
+
+        _wrap(query, arguments)
+
     def fetch_one(self, query, arguments = None):
         """
         Executes a query on the twisted thread-pool using the storm framework and returns the first result.
         :param query: The sql query to be executed.
         :param arguments: Optional arguments that go with the sql query.
-        :return: The first tuple that matches the query or None, would be the same as using execute
-        and calling the next() functions on it.
+        :return: A deferred that fires with the first tuple that matches the query or None.
+        The result would be the same as using execute and calling the next() function on it.
         """
-        return self.database._cursor.fetchone(query, arguments)
+        return self.transactor.run(self.database._cursor.fetchone(query, arguments))
