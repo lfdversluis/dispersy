fb3d3a7e2ce06b977d5fdf0e7fa24fc7883d38c9
Laurens Versluis <laurens-versluis@hotmail.com>
2016-07-01 18:38:43 +0200

    Refactored test_requestcache to handle the now asynchronous behavior of Dispersy

--- tests/test_requestcache.py
+++ tests/test_requestcache.py
@@ -1,6 +1,7 @@
+from .dispersytestclass import DispersyTestFunc
+
 from ..requestcache import RequestCache, NumberCache, RandomNumberCache
 from ..util import blocking_call_on_reactor_thread
-from .dispersytestclass import DispersyTestFunc
 
 
 class TestRequestCache(DispersyTestFunc):
