85f74627beceb4ad3d9069a007098f95e17f272d
Laurens Versluis <laurens-versluis@hotmail.com>
2016-07-01 18:35:50 +0200

    Refactored test_destroycommunity to handle the now asynchronous behavior of Dispersy

--- tests/test_destroycommunity.py
+++ tests/test_destroycommunity.py
@@ -1,8 +1,14 @@
+from nose.twistedtools import deferred, reactor
+from twisted.internet.defer import inlineCallbacks
+from twisted.internet.task import deferLater
+
 from .dispersytestclass import DispersyTestFunc
 
 
 class TestDestroyCommunity(DispersyTestFunc):
 
+    @deferred(timeout=15)
+    @inlineCallbacks
     def test_hard_kill(self):
         """
         Test that a community can be hard killed and their messages will be dropped from the DB.
@@ -11,29 +17,31 @@ class TestDestroyCommunity(DispersyTestFunc):
         3. MM destroys the community.
         4. Node wipes all messages from the community in the database.
         """
-        node, = self.create_nodes(1)
+        node, = yield self.create_nodes(1)
 
-        message = node.create_full_sync_text("Should be removed", 42)
-        node.give_message(message, node)
+        message = yield node.create_full_sync_text("Should be removed", 42)
+        yield node.give_message(message, node)
 
-        node.assert_count(message, 1)
+        yield node.assert_count(message, 1)
 
-        dmessage = self._mm.create_destroy_community(u"hard-kill")
+        dmessage = yield self._mm.create_destroy_community(u"hard-kill")
 
-        node.give_message(dmessage, self._mm)
+        yield node.give_message(dmessage, self._mm)
 
-        node.assert_count(message, 0)
+        yield node.assert_count(message, 0)
 
+    @deferred(timeout=10)
+    @inlineCallbacks
     def test_hard_kill_without_permission(self):
-        node, other = self.create_nodes(2)
-        node.send_identity(other)
+        node, other = yield self.create_nodes(2)
+        yield node.send_identity(other)
 
-        message = node.create_full_sync_text("Should not be removed", 42)
-        node.give_message(message, node)
+        message = yield node.create_full_sync_text("Should not be removed", 42)
+        yield node.give_message(message, node)
 
-        node.assert_count(message, 1)
+        yield node.assert_count(message, 1)
 
-        dmessage = other.create_destroy_community(u"hard-kill")
-        node.give_message(dmessage, self._mm)
+        dmessage = yield other.create_destroy_community(u"hard-kill")
+        yield node.give_message(dmessage, self._mm)
 
-        node.assert_count(message, 1)
+        yield node.assert_count(message, 1)
