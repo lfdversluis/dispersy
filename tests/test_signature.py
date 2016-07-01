af43242edef6902288c22c41509ea79be86fbee4
Laurens Versluis <laurens-versluis@hotmail.com>
2016-07-01 18:39:05 +0200

    Refactored test_signature to handle the now asynchronous behavior of Dispersy

--- tests/test_signature.py
+++ tests/test_signature.py
@@ -1,20 +1,24 @@
-from time import sleep
+from nose.twistedtools import deferred, reactor
+from twisted.internet.defer import inlineCallbacks
+from twisted.internet.task import deferLater
 
 from .dispersytestclass import DispersyTestFunc
 
 
 class TestSignature(DispersyTestFunc):
 
+    @deferred(timeout=10)
+    @inlineCallbacks
     def test_invalid_public_key(self):
         """
         NODE sends a message containing an invalid public-key to OTHER.
         OTHER should drop it
         """
-        node, other = self.create_nodes(2)
-        other.send_identity(node)
+        node, other = yield self.create_nodes(2)
+        yield other.send_identity(node)
 
-        message = node.create_bin_key_text('Should drop')
-        packet = node.encode_message(message)
+        message = yield node.create_bin_key_text('Should drop')
+        packet = yield node.encode_message(message)
 
         # replace the valid public-key with an invalid one
         public_key = node.my_member.public_key
@@ -24,26 +28,30 @@ class TestSignature(DispersyTestFunc):
         self.assertNotEqual(packet, invalid_packet)
 
         # give invalid message to OTHER
-        other.give_packet(invalid_packet, node)
+        yield other.give_packet(invalid_packet, node)
 
-        self.assertEqual(other.fetch_messages([u"bin-key-text", ]), [])
+        other_messages = yield other.fetch_messages([u"bin-key-text", ])
+        self.assertEqual(other_messages, [])
 
+    @deferred(timeout=10)
+    @inlineCallbacks
     def test_invalid_signature(self):
         """
         NODE sends a message containing an invalid signature to OTHER.
         OTHER should drop it
         """
-        node, other = self.create_nodes(2)
-        other.send_identity(node)
+        node, other = yield self.create_nodes(2)
+        yield other.send_identity(node)
 
-        message = node.create_full_sync_text('Should drop')
-        packet = node.encode_message(message)
+        message = yield node.create_full_sync_text('Should drop')
+        packet = yield node.encode_message(message)
 
         # replace the valid signature with an invalid one
         invalid_packet = packet[:-node.my_member.signature_length] + 'I' * node.my_member.signature_length
         self.assertNotEqual(packet, invalid_packet)
 
         # give invalid message to OTHER
-        other.give_packet(invalid_packet, node)
+        yield other.give_packet(invalid_packet, node)
 
-        self.assertEqual(other.fetch_messages([u"full-sync-text", ]), [])
+        other_messages = yield other.fetch_messages([u"full-sync-text", ])
+        self.assertEqual(other_messages, [])
